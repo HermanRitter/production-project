cc4f9ae20449c731d7d8ea3fd7c8740f307b6942
HermanRitter <gerka.rtr@yandex.ru>
2023-11-22 20:54:57 +0300

    initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Counter.module.scss'
+import classes from './Counter.module.scss'
+
+const Counter = () => {
+    return (
+        <div>
+            <div className={classes.counter}>asdasdasd</div>
+        </div>
+    );
+};
+
+export default Counter;
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function someFc(arg: number): string {
+    console.log('rondom 34556')
+
+    return ' '
+}
